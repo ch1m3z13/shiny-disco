b94960a8d9736ef54e9ed35615a07265e907a2e4
ch1m3z13 <chimeo81@gmail.com>
2024-12-19 09:24:04 +0100

    fixed breaking changes

--- config/database.js
+++ config/database.js
@@ -6,23 +6,23 @@ if (process.env.NODE_ENV === 'production') {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     logging: false, 
-    dialectOptions: {
-      ssl: {
-        require: true, 
-        rejectUnauthorized: true 
-      }
-    }
+   // dialectOptions: {
+     // ssl: {
+       // require: true, 
+       // rejectUnauthorized: true 
+     // }
+   // }
   });
 } else {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
         logging: true,
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false
-          }
-        }
+      //  dialectOptions: {
+        //  ssl: {
+           // require: false,
+        //    rejectUnauthorized: false
+        //  }
+      //  }
       });
 }
 
